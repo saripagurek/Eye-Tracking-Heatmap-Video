d5ba3297d836d89f0baae7d9a0442dfb4431e10e
saripagurek <saripvm@gmail.com>
2023-06-15 14:57:12 -0400

    Initial commit

---
+++ Heatmap/randcsv.py
@@ -0,0 +1,14 @@
+import csv
+import random
+
+rows = []
+
+for i in range(1400):
+    col = random.randint(0, 1400)
+    row = random.randint(0, 600)
+    rows.append([col, row])
+
+with open('datatest2.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    writer.writerows(rows)
